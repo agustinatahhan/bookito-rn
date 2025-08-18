503b684b240b03538758f7f378c7d8fbac674ca7
agustinatahhan <agustahhan@gmail.com>
2025-08-18 20:58:16 +0200

    installed tanstack query, created function to search book by query

---
+++ hooks/useBook.ts
@@ -0,0 +1,11 @@
+import { fetchBookSearch } from "@/services/bookSearch"
+import { Book } from "@/types/book"
+import { useQuery } from "@tanstack/react-query"
+export const useBookSearch = (query: string) => {
+    return useQuery<Book[]>({
+        queryKey: ["books", query],
+        queryFn: () => fetchBookSearch(query),
+        enabled: query.trim().length > 0,
+        staleTime: 1000 * 60 * 5
+    })
+}
